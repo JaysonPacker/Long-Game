ba2700861dfb92d6bb8363dc52b47bc49d315c56
JaysonPacker <jnp1380@rit.edu>
2024-11-10 13:50:56 -0500

    Finished assignment

--- Program.cs
+++ Program.cs
@@ -1,39 +1,62 @@
 ﻿namespace ConsoleApp6
 {
-
+    using System;
     using System.IO;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
 
     internal class Program
     {
         static void Main(string[] args)
         {
             // Varibles
-            string userName;
+            string userName = "unKnown";
             int score = 0;
-
+            string path;
+            bool play = true;
+            string line;
+           
 
             // ask for username
             Console.WriteLine("Please Enter Your username");
             userName = Console.ReadLine();
-
-            // make fileinfo ogject with username.txt as the math
-            FileInfo userFile = new FileInfo($@"C:\Users\Jayson P\source\repos\ConsoleApp6\userInfo\{userName}.txt");
-
+            // make path to {username}.txt
+            path = $@"..\userInfo\{userName}.txt";
+            // if the file exites the object opens it if not a new f
+            FileStream userFile = new FileStream(path, FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(userFile);
             // if there exist a file with the username use the score from that
-            if (userFile.Exists)
-            { 
+            line = sr.ReadLine();
+            if (line != null)
+            {               
+                score = Int32.Parse(sr.ReadLine());
                 Console.WriteLine("File Exists");
-            }else // if not make a file with that username  
-            {
-
+                sr.Close();
             }
 
-            // check for key pressed and incrment score and display it 
+            Console.WriteLine($"Your Current score: {score}");
+            Console.WriteLine("Let the game begin n/");
+            Console.WriteLine("Press Enter to end it");
 
-            // if the key is enter call endgame
-
-
-            Console.WriteLine("Hello, World!");
+            while (play)
+            {               
+            // check for key pressed 
+                if (Console.ReadKey().Key == ConsoleKey.Enter)// stop game if user preses enter
+                {
+                    play = false;
+                    Console.WriteLine($"Game over {userName}. Final Score {score}");
+                }
+                else// incrment score and display it
+                {
+                    score++;
+                    Console.WriteLine($" Your new Score: {score} ");
+                }
+            }
+            userFile.Close();
+              // write the score to the file
+            StreamWriter sw = new StreamWriter(path,false);
+            sw.WriteLine("Score");
+            sw.Write(score);
+            sw.Close();
         }
     }
 }
